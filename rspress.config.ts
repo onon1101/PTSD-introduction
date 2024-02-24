a499dd87b98a04a36c530a3bf1627c8633796e8c
onon1101 <mingzhuguly@gmail.com>
2024-02-24 11:06:40 +0800

    change baseurl

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,24 +1,28 @@
-import * as path from 'path';
-import { defineConfig } from 'rspress/config';
+import * as path from "path";
+import { defineConfig } from "rspress/config";
 
 export default defineConfig({
-  base: '/PTSD-introduction/',
-  root: path.join(__dirname, 'docs'),
-  title: 'Rspress',
-  description: 'Rspack-based Static Site Generator',
-  icon: '/rspress-icon.png',
+  // base: "/PTSD-introduction/",
+  builderConfig: {
+    output: {
+      assetPrefix: "https://onon.github.io/PTSD-introduction/",
+    },
+  },
+  root: path.join(__dirname, "docs"),
+  title: "Rspress",
+  description: "Rspack-based Static Site Generator",
+  icon: "/rspress-icon.png",
   logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
+    light: "/rspress-light-logo.png",
+    dark: "/rspress-dark-logo.png",
   },
   themeConfig: {
     socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+      {
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/web-infra-dev/rspress",
+      },
     ],
-  },
-  builderConfig: {
-    output: {
-      assetPrefix: 'https://onon1101.github.io/',
-    },
   },
 });
